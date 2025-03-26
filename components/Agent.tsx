bf822155968d7dd22915109962103699e53449ec
Avinash Gupta <guptaavinash302@gmail.com>
2025-03-26 13:44:59 +0530

    api setup done for vapi

---
+++ components/Agent.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import React from 'react'
+
+enum CallStatus {
+    INACTIVE = "INACTIVE",
+    CONNECTING = "CONNECTING",
+    ACTIVE = "ACTIVE",
+    FINISHED = "FINISHED",
+  }
+  
+  interface SavedMessage {
+    role: "user" | "system" | "assistant";
+    content: string;
+  }
+
+const Agent = ({userName}:AgentProps) => {
+    const isSpeaking = true;
+  return (
+    <div>
+        <div className="call-view">
+        {/* AI Interviewer Card */}
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interviewer</h3>
+        </div>
+
+        {/* User Profile Card */}
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Agent
